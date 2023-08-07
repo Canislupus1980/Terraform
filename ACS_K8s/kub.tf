d43915b6be86a49888891e0709bb660ba8f475b5
Siarhei Buhtiyarov <86954730+Canislupus1980@users.noreply.github.com>
2023-08-07 16:51:21 +0300

    Update kub.tf

--- ACS_K8s/kub.tf
+++ ACS_K8s/kub.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "cloudstack" {
-  api_url = "http://10.69.104.60:8080/client/api"
-  api_key = "vM-_I73vGvXu9g6eBbXJ3V48dySyGBLSrTyZhWBg5e7ztbg7A1YB938dq5-fVjialM--Cqu6zE_-A8seLwYx5w"
-  secret_key = "ir6GFaqspjQb4HaoJO7GFcc3csYe8UJXbGBkxEqhshwI6h14w5xXI22zDRvy2nCYNsj-sarvIWJAHG7VUeoIVw"  
+  api_url = ""
+  api_key = ""
+  secret_key = ""  
 }
 
 resource "cloudstack_instance" "k8s-cluster" {
